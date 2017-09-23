3000d951df69c650b590f03761c87671c4235d92
Jurek <jurek2006@gmail.com>
2017-09-23 16:23:51 +0200

    Działająca wersja bazująca na ukrywaniu elementów (na razie bez sprawdzania przypadku "za daleko")

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
 ( function() {
-    const numElementsVis = 3; //ilość widocznych elementów
+    let numElementsVis = 3; //ilość widocznych elementów
     let currEl = 0;
 
 
     // select elements
     const btnPrev = document.querySelector('.btn-prev');
     const btnNext = document.querySelector('.btn-next');
+    const btnPlus = document.querySelector('.btn-plus');
+    const btnMinus = document.querySelector('.btn-minus');
     const contBox = document.querySelector('.contBox');
-    const elBoxes = document.querySelectorAll('.elBox');
+    const elBoxes = contBox.children;
 
     // set event listeners 
     btnNext.addEventListener('click', () => moveElementLeft() );
     btnPrev.addEventListener('click', () => moveElementRight() );
+    btnPlus.addEventListener('click', () => expandSlider(numElementsVis + 1) );
+    
 
-    // number of all elements
+    const showElements = (firstElInd, numOfElem) => {
+        let elBoxesArr = [...elBoxes];
+
+        // ukrycie wszystkich elementów (nie najbardziej efektywne - poprawić)
+        for(let i = 0; i < elBoxesArr.length; i++){
+            elBoxesArr[i].style.display = "none";  
+        }
+
+        for(let i = firstElInd; i < firstElInd + numOfElem; i++ ){
+            elBoxesArr[i].style.display = "block";      
+        }
+    }
+
+    // na początku wszystkie elementy są ukryte
+    // więc trzeba odkryć numElementsVis (czyli zadane np. 3) elementów [od currEl]:
+    showElements(currEl, numElementsVis);
+
+    
+
+// number of all elements
     const countAllEl = () => elBoxes.length;
 
-    // indeks ostatniego aktualnie widocznego elementu
-    const lastVisElement = () => currEl + numElementsVis;
+    const moveElementLeft = () => showElements(++currEl, numElementsVis);
 
-    const moveElementLeft = () => {
-        if(lastVisElement() < countAllEl()){
-        // dopóki nie jest widoczny po prawej ostatni element - możliwe jest przesunięcie w prawo
-            currEl++;
-            moveElement();
-        }
-    }
+    const moveElementRight = () => showElements(--currEl, numElementsVis);
 
-    const moveElementRight = () => {
-        if(currEl>0){
-        // dopóki nie jest widoczny po prawej ostatni element - możliwe jest przesunięcie w prawo 
-            currEl--;
-            moveElement();
-        }
-    }
-
-    const moveElement = () => {
-        const translation = currEl * 102;
-        contBox.style.transform = `translateX(-${translation}px)`;
-        // console.log(translation);
+    const expandSlider = numOfElem => {
+        numElementsVis = numOfElem;
+        showElements(currEl, numElementsVis);
     }
 })();
+
+
