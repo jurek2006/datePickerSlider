94ce9b98ba6134f3de34ab188e51794166e0cac6
Jurek <jurek2006@gmail.com>
2017-09-23 14:18:02 +0200

    Działa przesuwanie elementu w prawo/lewo tak żeby nie przesunęło się "za daleko" (dla zdefiniowanej ilości elementów). Wielkości box'ów też zdefiniowane są na sztywno.

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 ( function() {
+    const numElementsVis = 3; //ilość widocznych elementów
     let currEl = 0;
+
+
     // select elements
     const btnPrev = document.querySelector('.btn-prev');
     const btnNext = document.querySelector('.btn-next');
     const contBox = document.querySelector('.contBox');
+    const elBoxes = document.querySelectorAll('.elBox');
 
     // set event listeners 
     btnNext.addEventListener('click', () => moveElementLeft() );
     btnPrev.addEventListener('click', () => moveElementRight() );
 
+    // number of all elements
+    const countAllEl = () => elBoxes.length;
+
+    // indeks ostatniego aktualnie widocznego elementu
+    const lastVisElement = () => currEl + numElementsVis;
+
     const moveElementLeft = () => {
-        currEl++;
-        moveElement();
+        if(lastVisElement() < countAllEl()){
+        // dopóki nie jest widoczny po prawej ostatni element - możliwe jest przesunięcie w prawo
+            currEl++;
+            moveElement();
+        }
     }
 
     const moveElementRight = () => {
-        currEl--;
-        moveElement();
+        if(currEl>0){
+        // dopóki nie jest widoczny po prawej ostatni element - możliwe jest przesunięcie w prawo 
+            currEl--;
+            moveElement();
+        }
     }
 
     const moveElement = () => {
         const translation = currEl * 102;
         contBox.style.transform = `translateX(-${translation}px)`;
-        console.log(translation);
+        // console.log(translation);
     }
 })();
